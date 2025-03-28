0633e15053b74d6648f25dcc1bfe8f7a02fb310e
pragathikiran <pragathi20.kiran@gmail.com>
2025-03-28 21:03:14 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.Screen().bgcolor("red")
+sc = turtle.Screen()
+sc.setup(400,400)
+turtle.title("TURTLE")
+board=turtle.Turtle()
+for i in range (4):
+ board .forward(300)
+ board.left(90)
+ i = i+1
